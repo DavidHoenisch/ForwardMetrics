e90bb04214bc1826da98cbcb399cfbd8f1af9d09
David Hoenisch <dh1689@pm.me>
2022-10-10 19:58:31 -0600

    Fixes for prophet functionality

--- app/website/docs.py
+++ app/website/docs.py
@@ -1,3 +1,4 @@
+from pydoc import render_doc
 from flask import Blueprint, render_template
 
 docs = Blueprint("docs", __name__)
@@ -10,5 +11,9 @@ def docs_table():
 @docs.route("/upload_predictions")
 def upload_predictions():
     return render_template("upload_predictions.html")
+
+@docs.route("/alpha_vantage")
+def alpha_vantage():
+    return render_template("alpha_vantage_guide.html")
 
 
