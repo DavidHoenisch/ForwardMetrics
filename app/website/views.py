bba58ca586542f29a0b4c190326a2fcdfbb7687c
David Hoenisch <dh1689@pm.me>
2022-11-11 19:47:43 -0700

    changes for alpha release:  home.html, main.css, and images folder

--- app/website/views.py
+++ app/website/views.py
@@ -6,8 +6,3 @@ views = Blueprint("views", __name__)
 @views.route("/")
 def home():
     return render_template("home.html")
-
-@views.route("/about")
-def about():
-    return render_template("about.html")
-
