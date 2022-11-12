394e19bc2415a07f85587193dc3d5046df0b393c
David Hoenisch <dh1689@pm.me>
2022-11-11 20:27:02 -0700

    made the server production ready using waitress.serve

--- app/app.py
+++ app/app.py
@@ -2,6 +2,6 @@ from website import forwardmetrics
 
 
 if __name__ == "__main__":
+    from waitress import serve
+    serve(forwardmetrics(), host="0.0.0.0", port=5000)
     
-    app = forwardmetrics()
-    app.run(debug=True)
