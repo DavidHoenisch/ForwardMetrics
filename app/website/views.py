a4bcb09a12d5bafe35dd981be2ad75b9e94d2d08
David Hoenisch <dh1689@pm.me>
2023-02-16 22:27:35 -0700

    Added in fixes for nav bar

--- app/website/views.py
+++ app/website/views.py
@@ -1,12 +1,13 @@
 from flask import Blueprint, render_template
 
-
 views = Blueprint("views", __name__)
+
 
 @views.route("/home")
 @views.route("/")
 def home():
     return render_template("home.html")
+
 
 @views.route("/login")
 @views.route("/signup")
